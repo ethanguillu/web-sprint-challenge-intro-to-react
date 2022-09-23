6e8020dbff2c6b40c278a3e7841caeccc2c2f348
ethanguillu <109877529+ethanguillu@users.noreply.github.com>
2022-09-23 12:21:21 -0500

    Finished Sprint Challenge

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,19 @@
 // Write your Character component here
+import React, { useState, useEffect} from "react";
+import axios from "axios";
+
+const Character = (props) => {
+    return (
+        <div>
+            <h1 className="Header">Characters</h1>
+            <h2>{props.charInfo[0].name}</h2>
+            <h2>{props.charInfo[1].name}</h2>
+            <h2>{props.charInfo[2].name}</h2>
+            <h2>{props.charInfo[3].name}</h2>
+            <h2>{props.charInfo[4].name}</h2>
+            <h2>{props.charInfo[5].name}</h2>
+        </div>
+    )
+}
+
+export default Character;
